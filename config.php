c0290312168d2f67d918c3a7eaca4957e6607044
sucrema <47716655+sucrema@users.noreply.github.com>
2019-02-17 22:21:24 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 //支付宝链接
-$alipay_url = 'https://qr.alipay.com/tsx099685ffdf96nzasre20';
+$alipay_url = 'https://qr.alipay.com/fkx01434hgduhfvaay1exe1';
 //微信支付链接
-$wechat_url = 'https://payapp.weixin.qq.com/qr/AQGXFj0KWP93Es0y3w5rYzdY?t=CwAK#wechat_pay';
+$wechat_url = 'wxp://f2f0Oa0lMFGmXitmtFeTjER_jVAgQt7kMXzt';
 //qq支付链接
-$qq_url     = 'http://vac.qq.com/wallet/qrcode.htm?m=tenpay&a=1&u=839488083&ac=7B411D7D0588EBECADFAF6E63B7619DE1B6906DDFD7C1B9919FF8442F8A7C5C0&n=%E5%B0%8F%E5%B0%8F%E9%85%A5&f=wallet';
-//京东金融支付链接
-$jd_url     = 'https://sq.jd.com/pay/Zx1fZ4';
+$qq_url     = 'https://i.qianbao.qq.com/wallet/sqrcode.htm?m=tenpay&a=1&u=1113298532&ac=EE8480EC250FC2F272E33D7EF27A7E4AC6380EE2C8E05E000ED2B1536E5A1F70&n=%E2%80%8F&f=wallet';
+//银联云闪付链接
+$95516_url     = 'https://qr.95516.com/00010000/01163935581485902177604445115857';
